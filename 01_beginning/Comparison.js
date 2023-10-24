1eaa68270f4bef83cfe6f465bd169837a94922a8
SumanKunwar1 <148757875+SumanKunwar1@users.noreply.github.com>
2023-10-24 09:13:39 +0000

    comparison

---
+++ 01_beginning/Comparison.js
@@ -0,0 +1,11 @@
+console.log(2>=1)
+
+//javascript allow the comprison between differnt data type
+console.log("2">=1) // here simply it convert string into number and compare
+//but while comparaing we have to make sure that we are comparing among same datatype--for good practise
+console.log(null>0)
+console.log(null==0)
+console.log(null<0)
+//thease two value only return true and rest of other return false
+console.log(null>=0)
+console.log(null<=0)
---
+++ 01_beginning/operation.js
@@ -0,0 +1,24 @@
+let val=3
+let negVal= -val
+console.log(negVal)
+
+let Str1="Hello"
+let str2 = " Suman"
+
+let str3= Str1+ str2
+console.log(str3)
+/* if we try to add string and num it concatination will perfom because num are immediately
+convert into string*/
+console.log(2+"1")
+console.log("1"+2+2)
+
+//but at the same time if we write multiple number and then string first calculation will perfom then 
+// concatination happen
+console.log(2+2+"1")
+//use paranthesis try not to make code confusing as much as possible 
+console.log((4+3)*5%9)
+//not important but we can convert null or boolean into number in this way
+console.log(+true)
+
+
+
