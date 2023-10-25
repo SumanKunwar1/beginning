c0fada29f0ef401dd3fb5e226ad50f429bc11d1e
SumanKunwar1 <148757875+SumanKunwar1@users.noreply.github.com>
2023-10-25 15:15:07 +0000

    d

---
+++ Object/Object2.js
@@ -0,0 +1,9 @@
+//destructuring 
+
+const course={
+    cousre:"Javascript",
+    fee:9999,
+    Teacher:"hitesh"
+}
+const {Teacher:Instructor}=course
+console.log(Instructor);//this is destructuring
