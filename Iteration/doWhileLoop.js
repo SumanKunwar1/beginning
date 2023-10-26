3469657a6b95fb9f4085ee9e3e09e39529ccceb1
SumanKunwar1 <148757875+SumanKunwar1@users.noreply.github.com>
2023-10-26 10:36:45 +0000

    loop

---
+++ Iteration/doWhileLoop.js
@@ -0,0 +1,19 @@
+let index =1
+while (index<=10) {
+    //console.log(index);
+    index=index+2
+}
+let myArr=["flash","batman","ironman","spiderman"]
+let arr=0
+while(arr<myArr.length){
+    //console.log(`value is ${myArr[arr]}`);
+    arr++
+}
+
+//do-while loop- at least once loop run on 
+let score=11 
+do {
+    console.log(`score : ${score}`);
+    score++
+    
+} while (score<=10);
